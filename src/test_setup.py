bca5fee37d901e2f876a202f9bca21a57627dd46
Alex Nguyen <alexnguyen7369@gmail.com>
2026-05-21 11:44:59 -0700

    Initial computer vision project setup

---
+++ src/test_setup.py
@@ -0,0 +1,9 @@
+import torch
+import cv2
+import PIL
+import faiss
+import transformers
+
+print("Environment is working.")
+print("PyTorch version:", torch.__version__)
+print("OpenCV version:", cv2.__version__)
